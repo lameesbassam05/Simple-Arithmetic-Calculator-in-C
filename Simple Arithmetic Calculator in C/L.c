15ddeb2fd5ba0a021f3ba16169bda7a7baacd1df
Lamees Bassam Saleh <lamees.bassam05@gmail.com>
2025-09-13 17:45:20 +0300

    Add files via upload

--- Simple Arithmetic Calculator in C/L.c
+++ Simple Arithmetic Calculator in C/L.c
@@ -1,5 +1,28 @@
-#include <stdio.h> 
-int main () {
-   printf("free palistine"); /* اطبع free palistine */
-   return 0; /*انتهاء البرنامج*/
+#include <stdio.h>
+int main(void)
+{
+    int num1 = 0, num2 = 0; // عرف المتغيرين بصفر
+
+    printf("Enter number: \n"); // اطبع ادخل الرقم 1
+    scanf("%d", &num1);         // لادخال القيمة 1
+
+    printf("Enter other number: \n"); // اطبع ادخل الرقم 2
+    scanf("%d", &num2);               // لادخال القيمة 2
+
+    printf("The sum is: %d\n", num1 + num2); // اطبع ان المجموع حاصل جمع المتغيرين
+    printf("%d + %d = %d\n\n", num1, num2, num1 + num2);
+
+    printf("The defferance is: %d\n", num1 - num2); // طرح
+    printf("%d - %d = %d\n\n", num1, num2, num1 - num2);
+
+    printf("The product is: %d\n", num1 * num2); // ضرب
+    printf("%d * %d = %d\n\n", num1, num2, num1 * num2);
+
+    printf("The quotient is: %d\n", num1 / num2); // قسمة
+    printf("%d / %d = %d\n\n", num1, num2, num1 / num2);
+
+    printf("The remainder is: %d\n", num1 % num2); // النسبة المئوية
+    printf("%d %% %d = %d\n\n", num1, num2, num1 % num2);
+
+    return 0;
 }
